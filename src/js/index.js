391e5b5fdf9dd929197e75e3f4b9c68454ef5ec2
ChrissaK <chrissa.kaza@gmail.com>
2020-04-29 17:52:07 +0100

    Edited Recipe class and added Recipe View

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
-import * as searchView from './views/searchView'
+import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -17,7 +18,7 @@ const state = {};
 const controlSearch = async () => {
     //1. Get query from view
     // const query = 'pizza'; //TODO
-    const query = searchView.getInput(); //TODO
+    const query = searchView.getInput(); 
 
     if (query) {
         //2. New search object and add to state object
@@ -28,12 +29,19 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        //4. Search for recipes
-       await state.search.getResults();
+        try {
+            //4. Search for recipes
+            await state.search.getResults();
 
-        //5. Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            //5. Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+
+        } catch (err) {
+            alert('Something went wrong with the search...');
+            clearLoader();
+        }
+
         
     }
 
@@ -43,6 +51,12 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
+
+//for TESTING
+// window.addEventListener('load', e => {
+//     e.preventDefault();
+//     controlSearch();
+// });
 
 elements.searchResPages.addEventListener('click', e => {
     const btn = e.target.closest('.btn-inline'); //closest method finds closest ancestor with the class
@@ -57,7 +71,44 @@ elements.searchResPages.addEventListener('click', e => {
 /**
  * RECIPE Controller 
  */
-const r = new Recipe(46956);
-r.getRecipe();
-console.log(r);
+const controlRecipe = async () => {
+    // Get ID from the url
+    const id = window.location.hash.replace('#','');
+    console.log(id);
+
+    if (id) {
+        // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+        
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        // TESTING
+        // window.r = state.recipe;
+
+        try {
+            // Get recipe data & parse ingredients (load recipe data in the background, getRecipe is async so will return a Promise)
+            await state.recipe.getRecipe(); 
+            state.recipe.parseIngredients();
+
+            // Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // Render recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
+        } catch (err) {
+            alert('Error processing recipe :(')
+        }
+
+
+        
+
+    }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -29,4 +29,63 @@ export default class Recipe {
     calcServings() {
         this.servings = 4;
     }
+
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons','teaspoon', 'cups', 'pounds'];
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const units = [...unitsShort, 'g', 'kg'];
+
+        const newIngredients = this.ingredients.map(el => {
+            // 1. Uniform units
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitsShort[i]);
+            });
+
+            // 2. Remove ()
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' '); 
+
+            // 3. Parse ingredients into count, unit and ingredient
+            const arrIng = ingredient.split(' ');
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
+            
+            let objIng;
+
+            if (unitIndex > -1) {
+                // There is a unit
+                const arrCount = arrIng.slice(0, unitIndex);
+                let count;
+
+                if (arrCount.length === 1) {
+                    count = parseFloat(eval(arrIng[0].replace('-','+')).toFixed(2));
+                } else {
+                    count = parseFloat(eval(arrIng.slice(0, unitIndex).join('+')).toFixed(2));
+                }
+
+                objIng = {
+                    count: count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex + 1).join(' ')
+                }
+            
+            } else if (parseInt(arrIng[0], 10)) {
+                // There is NO unit, but 1st element is a number (eg.1 package)
+                objIng = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ') //the entire array except for the 1st element
+                }
+            } else if (unitIndex === -1) {
+                // There is no unit
+                objIng = {
+                    count: 1,
+                    unit: '',
+                    ingredient: ingredient
+                }
+            }
+
+            return objIng;
+        });
+        this.ingredients = newIngredients;
+    }
 }
